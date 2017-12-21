59a28e2db528f716d81bcd1e4417d82d16d0c30a
Nikolai <Nikolai@website.com>
2017-12-21 02:13:46 +0200

    v1.1.1.2.2.3

---
+++ MoviesForRent/MoviesForRent/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoviesForRent.Models;
+
+namespace MoviesForRent.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            //return View(movie); // !!!
+            //return new RedirectResult("https://www.google.com/"); // !!!
+            //return HttpNotFound(); // !!!
+            //return Content("Hello World");
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+
+            return View(movie);
+        }
+    }
+}
